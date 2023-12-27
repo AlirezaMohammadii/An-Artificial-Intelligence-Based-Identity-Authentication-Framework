f688004fba76ac31de2af714f78baa6924c8d95a
Ali <novamova.ali@gmail.com>
2023-12-27 12:41:39 +1100

    A more comprehensive version with preprocessing and randomizing audio files capability all in one file

--- voiceTrimmer.py
+++ voiceTrimmer.py
@@ -1,44 +1,67 @@
 import os
 from pydub import AudioSegment
 import shutil
+import random
+import concurrent.futures
+from pydub import AudioSegment
 
-# Set the paths to ffmpeg and ffprobe (Replace with the actual paths if needed)
-# ffmpeg_path = "C:/Users/Shaygan/AppData/Local/ffmpegio/ffmpeg-downloader/ffmpeg/bin"
-# ffprobe_path = "C:/Users/Shaygan/AppData/Local/ffmpegio/ffmpeg-downloader/ffmpeg/bin"
 
-# Set the paths for pydub (Uncomment and modify if using ffmpeg and ffprobe paths)
-# AudioSegment.converter = ffmpeg_path
-# AudioSegment.ffmpeg = ffmpeg_path
-# AudioSegment.ffprobe = ffprobe_path
+def get_all_files(directory):
+    all_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            all_files.append(os.path.join(root, file))
+    return all_files
+
+
+def prepare_destination_directories(parent_directory):
+    subdirectories = [
+        d
+        for d in os.listdir(parent_directory)
+        if os.path.isdir(os.path.join(parent_directory, d))
+    ]
+    for subdirectory in subdirectories:
+        destination_path = os.path.join(parent_directory, subdirectory)
+        os.makedirs(destination_path, exist_ok=True)
+
+
+def move_files_to_destinations(files, parent_directory):
+    for file in files:
+        destination_directory = os.path.join(
+            parent_directory, file.split(os.path.sep)[1]
+        )
+        destination_path = os.path.join(destination_directory, os.path.basename(file))
+        os.replace(file, destination_path)
 
 
 def delete_short_audio_files(directory, duration_threshold=7):
-    """
-    Deletes short audio files within a specified directory and its subdirectories.
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
 
-    Parameters:
-    - directory (str): The path to the parent directory containing audio files.
-    - duration_threshold (float): The minimum duration (in seconds) for an audio file to be retained.
-                                  Defaults to 7 seconds.
-
-    Returns:
-    - None
-    """
-
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.lower().endswith(".flac"):
-                file_path = os.path.join(root, file)
-                audio = AudioSegment.from_file(file_path, format="flac")
-                duration_in_seconds = (
-                    len(audio) / 1000
-                )  # Convert milliseconds to seconds
-
-                if duration_in_seconds < duration_threshold:
-                    print(
-                        f"Deleting {file_path} with duration {duration_in_seconds} seconds."
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.lower().endswith((".flac", ".wav")):
+                    file_path = os.path.join(root, file)
+                    futures.append(
+                        executor.submit(
+                            process_audio_file, file_path, duration_threshold
+                        )
                     )
-                    os.remove(file_path)
+
+        # Wait for all threads to complete
+        concurrent.futures.wait(futures)
+
+
+def process_audio_file(file_path, duration_threshold):
+    try:
+        audio = AudioSegment.from_file(file_path)
+        duration_in_seconds = len(audio) / 1000
+
+        if duration_in_seconds < duration_threshold:
+            os.remove(file_path)
+    except Exception as e:
+        # Handle exceptions (e.g., invalid audio file)
+        pass
 
 
 def delete_matrices(directory_path):
@@ -65,19 +88,42 @@ def delete_files_except_flac(directory_path):
                 os.remove(file_path)
 
 
-if __name__ == "__main__":
-    # Specify the parent directory path where short audio files will be deleted
-    parent_directory = "F:/1.Deakin university/Python/13_10_2023_My_Project_1/Deep-VR/deep-speaker/samples/LibriSpeech/dev-clean"
+def keep_random_files(directory, num_files_to_keep=10):
+    for root, dirs, files in os.walk(directory):
+        if files and any(file.lower().endswith(".flac") for file in files):
+            files_to_keep = random.sample(files, min(num_files_to_keep, len(files)))
 
-    # Call the function to delete short audio files
-    # delete_short_audio_files(parent_directory)
+            for file in files:
+                file_path = os.path.join(root, file)
+                if file not in files_to_keep:
+                    print(f"Deleting {file_path}")
+                    os.remove(file_path)
 
-    # To delete directories within files
-    # if not os.path.exists(parent_directory):
-    #     print("Directory not found.")
-    # else:
-    #     delete_matrices(parent_directory)
-    #     print("Deletion process completed.")
 
-    # Call the function to delete files
+def delete_empty_subdirectories(directory_path):
+    for root, dirs, files in os.walk(directory_path, topdown=False):
+        for directory in dirs:
+            dir_path = os.path.join(root, directory)
+            if not os.listdir(dir_path):
+                print(f"Deleting empty directory: {dir_path}")
+                os.rmdir(dir_path)
+
+
+def main(parent_directory):
+    all_files = get_all_files(parent_directory)
+    prepare_destination_directories(parent_directory)
+    move_files_to_destinations(all_files, parent_directory)
+
+    # Additional functions
+    # delete_short_audio_files(parent_directory, duration_threshold=7)
+    keep_random_files(parent_directory)
+    delete_matrices(parent_directory)
     delete_files_except_flac(parent_directory)
+    delete_empty_subdirectories(parent_directory)
+
+
+# Example Usage:
+parent_dir = (
+    "F:/1.Deakin university/Python/13_10_2023_My_Project_1/Deep-VR/dev-clean-original"
+)
+main(parent_dir)
