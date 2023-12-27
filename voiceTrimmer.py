1dff69d86096204a533bccc398fe5f31773f3d31
Ali <novamova.ali@gmail.com>
2023-12-27 21:33:26 +1100

    better version with measurment of how long the script takes to complete a run

--- voiceTrimmer.py
+++ voiceTrimmer.py
@@ -1,20 +1,35 @@
 import os
-from pydub import AudioSegment
 import shutil
 import random
 import concurrent.futures
 from pydub import AudioSegment
+from pathlib import Path
 
 
 def get_all_files(directory):
+    """
+    Get a list of all file paths in the specified directory and its subdirectories.
+
+    Args:
+        directory (str): The path to the directory.
+
+    Returns:
+        list: A list of file paths.
+    """
     all_files = []
-    for root, dirs, files in os.walk(directory):
+    for root, _, files in os.walk(directory):
         for file in files:
             all_files.append(os.path.join(root, file))
     return all_files
 
 
 def prepare_destination_directories(parent_directory):
+    """
+    Create subdirectories in the parent directory based on existing directories.
+
+    Args:
+        parent_directory (str): The path to the parent directory.
+    """
     subdirectories = [
         d
         for d in os.listdir(parent_directory)
@@ -26,6 +41,13 @@ def prepare_destination_directories(parent_directory):
 
 
 def move_files_to_destinations(files, parent_directory):
+    """
+    Move files to destination directories based on their immediate parent directory.
+
+    Args:
+        files (list): List of file paths.
+        parent_directory (str): The path to the parent directory.
+    """
     for file in files:
         destination_directory = os.path.join(
             parent_directory, file.split(os.path.sep)[1]
@@ -35,10 +57,17 @@ def move_files_to_destinations(files, parent_directory):
 
 
 def delete_short_audio_files(directory, duration_threshold=7):
+    """
+    Delete audio files shorter than a specified duration.
+
+    Args:
+        directory (str): The path to the directory.
+        duration_threshold (int): The minimum duration threshold in seconds.
+    """
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = []
 
-        for root, dirs, files in os.walk(directory):
+        for root, _, files in os.walk(directory):
             for file in files:
                 if file.lower().endswith((".flac", ".wav")):
                     file_path = os.path.join(root, file)
@@ -53,34 +82,52 @@ def delete_short_audio_files(directory, duration_threshold=7):
 
 
 def process_audio_file(file_path, duration_threshold):
+    """
+    Process audio files and delete those below the duration threshold.
+
+    Args:
+        file_path (Path): The path to the audio file.
+        duration_threshold (int): The minimum duration threshold in seconds.
+    """
     try:
         audio = AudioSegment.from_file(file_path)
         duration_in_seconds = len(audio) / 1000
 
         if duration_in_seconds < duration_threshold:
             os.remove(file_path)
+    except FileNotFoundError:
+        pass  # Handle file not found exception
     except Exception as e:
-        # Handle exceptions (e.g., invalid audio file)
-        pass
+        print(f"Error processing {file_path}: {e}")
 
 
 def delete_matrices(directory_path):
-    for root, dirs, files in os.walk(directory_path, topdown=False):
-        for directory in dirs:
-            if directory.lower() == "matrices":
-                matrices_path = os.path.join(root, directory)
-                shutil.rmtree(matrices_path)
-                print(f"Deleted directory: {matrices_path}")
+    """
+    Delete directories and files with the name 'matrices'.
 
-        for file in files:
-            if file.lower() == "matrices":
-                matrices_path = os.path.join(root, file)
-                os.remove(matrices_path)
-                print(f"Deleted file: {matrices_path}")
+    Args:
+        directory_path (str): The path to the directory.
+    """
+    for root, dirs, files in os.walk(directory_path, topdown=False):
+        for item in dirs + files:
+            item_path = os.path.join(root, item)
+            if item.lower() == "matrices":
+                if os.path.isdir(item_path):
+                    shutil.rmtree(item_path)
+                    print(f"Deleted directory: {item_path}")
+                else:
+                    os.remove(item_path)
+                    print(f"Deleted file: {item_path}")
 
 
 def delete_files_except_flac(directory_path):
-    for root, dirs, files in os.walk(directory_path):
+    """
+    Delete files in a directory except those with the '.flac' extension.
+
+    Args:
+        directory_path (str): The path to the directory.
+    """
+    for root, _, files in os.walk(directory_path):
         for file in files:
             file_path = os.path.join(root, file)
             if not file.endswith(".flac"):
@@ -88,19 +135,53 @@ def delete_files_except_flac(directory_path):
                 os.remove(file_path)
 
 
-def keep_random_files(directory, num_files_to_keep=10):
-    for root, dirs, files in os.walk(directory):
-        if files and any(file.lower().endswith(".flac") for file in files):
-            files_to_keep = random.sample(files, min(num_files_to_keep, len(files)))
+def keep_random_files(directory_path, seed=None):
+    """
+    Randomly keeps exactly 10 '.flac' files in each subdirectory within the given main directory,
+    and deletes the rest. The randomization is reproducible if a seed is provided.
 
-            for file in files:
-                file_path = os.path.join(root, file)
-                if file not in files_to_keep:
-                    print(f"Deleting {file_path}")
-                    os.remove(file_path)
+    Parameters:
+        main_directory (str): The main directory path.
+        seed (int): Seed for reproducibility in randomization. Default is None.
+
+    Raises:
+        ValueError: If the provided main_directory is not a valid directory.
+
+    Returns:
+        None
+    """
+    # Set seed for reproducibility
+    random.seed(seed)
+
+    # Validate and convert input to Path object
+    main_directory = Path(directory_path)
+    if not main_directory.is_dir():
+        raise ValueError("The provided main_directory is not a valid directory.")
+
+    # Iterate through subdirectories
+    for subdirectory in main_directory.iterdir():
+        if subdirectory.is_dir():
+            # Get all .flac files in the subdirectory
+            flac_files = list(subdirectory.glob("*.flac"))
+
+            # Check if there are more than 10 .flac files
+            if len(flac_files) > 10:
+                # Randomly select 10 files
+                files_to_keep = random.sample(flac_files, 10)
+
+                # Delete the rest of the files
+                for file in flac_files:
+                    if file not in files_to_keep:
+                        file.unlink()
 
 
 def delete_empty_subdirectories(directory_path):
+    """
+    Delete empty subdirectories in the specified directory.
+
+    Args:
+        directory_path (str): The path to the directory.
+    """
     for root, dirs, files in os.walk(directory_path, topdown=False):
         for directory in dirs:
             dir_path = os.path.join(root, directory)
@@ -110,20 +191,33 @@ def delete_empty_subdirectories(directory_path):
 
 
 def main(parent_directory):
+    """
+    Perform main operations on the specified parent directory.
+
+    Args:
+        parent_directory (str): The path to the parent directory.
+    """
     all_files = get_all_files(parent_directory)
     prepare_destination_directories(parent_directory)
     move_files_to_destinations(all_files, parent_directory)
 
     # Additional functions
-    # delete_short_audio_files(parent_directory, duration_threshold=7)
-    keep_random_files(parent_directory)
-    delete_matrices(parent_directory)
+    delete_short_audio_files(parent_directory, duration_threshold=7)
+    keep_random_files(parent_directory, seed=43)
+    # delete_matrices(parent_directory)
     delete_files_except_flac(parent_directory)
     delete_empty_subdirectories(parent_directory)
 
 
-# Example Usage:
-parent_dir = (
-    "F:/1.Deakin university/Python/13_10_2023_My_Project_1/Deep-VR/dev-clean-original"
-)
-main(parent_dir)
+if __name__ == "__main__":
+    # Example Usage:
+    parent_dir = "F:/1.Deakin university/Python/13_10_2023_My_Project_1/Deep-VR/LibriSpeech/train-clean-100"
+
+    import time
+
+    start_time = time.time()
+    main(parent_dir)
+    end_time = time.time()
+
+    # Print execution time
+    print(f"Execution time: {end_time - start_time} seconds")
